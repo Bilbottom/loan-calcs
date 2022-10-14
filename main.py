702e1d6862f83241f75abf1342f39922601b556e
Bill <Bill@TheWallisFamily.co.uk>
2022-10-14 20:59:41 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Similar packages:
+* https://loan-calculator.readthedocs.io/en/latest/index.html
+* https://mortgage.readthedocs.io/en/latest/index.html
+"""
+import loan_calcs
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
